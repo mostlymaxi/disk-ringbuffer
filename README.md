b26ccd07c7128ff4e972e2a1d00ba1991aca5c04
Max Saparov <max.saparov@gmail.com>
2024-06-26 17:20:54 -0400

    working prototype

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # On Disk Ringbuffer
+
+### Layer 0
+What is RSV???
+
+a, b, c - comma seperated values
+0x61 0x2c 0x62 0x2c 0x63 - in unicode
+     0xFF      0xFF
+
+### Layer 1
+What are pages (not unix pages - MY pages)
+pages are data structures
+
+is_ready long - not used
+write_idx - where are we writing
+another_write_idx - the last confirmed safe write
+and_another_write_idx - not used
+empty space - this is the empty space where we actually write stuff
+
+0000 0000 - is_ready
+0000 0000 - write_idx
+0000 0000 - another write idx
+0000 0000 - and anotheru
+
+0000000000000000000000000000000000000000000000000000000000000000000000000000000000
+
+0000 0000 + 3 = 0000 0012 - write_idx
+00000000000000000000000000000000000000000000000000000000000000000000000000000000000
+00000000000abc000000000000000000000000000000000000000000000000000000000000000000000
+0000 0000 + 3 = 0000 0003 - finished_write_idx
+
+0000 0000 + 1 0003 = 0001 0012 - write_idx
+abc0xffdef0xffghi0xff000000000000000000000000000000000000000000000000000000000000
+
+0001 0003 - 1 0000 = 0000 0003 - write_idx
+
+1111 0000
+    &
+0000 0010  0010 0100
+-> max(safe_write_idx, 0000 0012) -> safe write idx
+
+0000 0003
+
+write_idx < read_idx + length => no new data / empty read
+
+
--- src/ringbuf.rs
+++ src/ringbuf.rs
@@ -1,55 +1,94 @@
 use crate::page::{Page, ReadResult};
 use std::path::PathBuf;
+use std::sync::{Arc, RwLock};
 
 pub struct Ringbuf {
     name: PathBuf,
-    write_page_count: usize,
+    // write page count should be atomic and set to max of increment when incrementing?
+    write_page_count: Arc<RwLock<usize>>,
+    write_page_no: usize,
     write_page: Page,
     read_page_count: usize,
     read_page: Page,
     read_start_byte: usize,
+    max_total_pages: usize,
 }
 
 impl Ringbuf {
     pub fn new<P: Into<PathBuf>>(path: P) -> Ringbuf {
+        const MAX_TOTAL_PAGES: usize = 3;
         let name = path.into();
         let _ = std::fs::create_dir_all(&name);
 
         Ringbuf {
             name: name.clone(),
-            write_page_count: 0,
+            write_page_count: Arc::new(RwLock::new(0)),
+            write_page_no: 0,
             read_page_count: 0,
             read_start_byte: 0,
+            max_total_pages: MAX_TOTAL_PAGES,
             // should open lowest number page in the directory rather than 0
             write_page: Page::new(&name.join("0.test.bin").to_string_lossy()),
             read_page: Page::new(&name.join("0.test.bin").to_string_lossy()),
         }
     }
 
+    fn page_flip(&mut self) {
+        let page_count = self
+            .write_page_count
+            .read()
+            .expect("something went really bad with your lock");
+
+        if *page_count == self.write_page_no {
+            drop(page_count);
+
+            let mut page_count = self
+                .write_page_count
+                .write()
+                .expect("something went really bad with your lock");
+
+            if *page_count < self.write_page_no {
+                self.write_page_no += 1;
+                return;
+            }
+
+            *page_count += 1;
+            self.write_page_no += 1;
+
+            if *page_count >= self.max_total_pages {
+                std::fs::remove_file(
+                    &self
+                        .name
+                        .join(format!("{}.test.bin", *page_count - self.max_total_pages)),
+                )
+                .expect("something went wrong deleting an old file");
+            }
+        }
+    }
+
     pub fn push<T: AsRef<[u8]>>(&mut self, input: T) {
         loop {
             let _ = match self.write_page.try_push(&input) {
-                Ok(0) => 0,
+                Ok(0) => 0, // PAGE FULL / Continue
                 Ok(_) => break,
                 Err(e) => panic!("{:#?}", e),
             };
 
-            self.write_page_count += 1;
+            self.page_flip();
+
             self.write_page = Page::new(
                 &self
                     .name
-                    .join(format!("{}.test.bin", self.write_page_count))
+                    .join(format!("{}.test.bin", self.write_page_no))
                     .to_string_lossy(),
             );
         }
     }
 
     pub fn pop(&mut self) -> Option<String> {
-        const SOME_NUMBER: usize = 2;
-
         loop {
             match self.read_page.try_pop(self.read_start_byte) {
-                Ok(None) => return None,
+                Ok(None) => return None, // no new messages
                 Ok(Some(ReadResult::Continue)) => {}
                 Ok(Some(ReadResult::Msg(m))) => {
                     self.read_start_byte += m.len() + 1;
@@ -58,13 +97,14 @@ impl Ringbuf {
                 Err(e) => panic!("{e}"),
             };
 
-            for i in self.read_page_count..self.write_page_count.saturating_sub(SOME_NUMBER) {
-                let _ = std::fs::remove_file(&self.name.join(format!("{}.test.bin", i)));
-            }
+            let page_count = self
+                .write_page_count
+                .read()
+                .expect("something went really wrong with your lock");
 
             self.read_page_count = std::cmp::max(
                 self.read_page_count + 1,
-                self.write_page_count.saturating_sub(SOME_NUMBER),
+                page_count.saturating_sub(self.max_total_pages),
             );
 
             self.read_start_byte = 0;
@@ -78,24 +118,21 @@ impl Ringbuf {
     }
 }
 
-// gotta make ringbuf a c struct cri
-
 #[test]
 fn ringbuf_sequential_test() {
     let mut r = Ringbuf::new("test");
 
+    let now = std::time::Instant::now();
     for i in 0..50_000_000 {
         r.push(i.to_string());
     }
 
-    for i in 0..10_000_000 {
+    for i in 0..50_000_000 {
         let m = r.pop().unwrap();
         assert_eq!(m, i.to_string());
     }
 
-    for _ in 10_000_000..50_000_000 {
-        let _ = r.pop().unwrap();
-    }
+    eprintln!("took {} ms", now.elapsed().as_millis());
 }
 // deleting pages on pop makes life much easier as opposed to deleting
 // old pages on push which might screw things up
